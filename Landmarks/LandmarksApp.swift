707d39556519c377a4081889c1fd3baa0a6c8a81
Andres Berdugo <berrugo@gmail.com>
2023-01-03 21:10:47 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Andres Berdugo on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
